7479d319e78515f7e959980a6f58b8f6317bb12f
JeevaBabuM <127578788+JeevaBabuM@users.noreply.github.com>
2023-04-06 06:22:33 +0000

    Calucalator task completed

--- src/components/Applications/Calculator.js
+++ src/components/Applications/Calculator.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Calculator() {
+    const [val,setVal] = useState('')
+    const [result, setResult] =  useState('')
+
+    const symbols = ['+','-','*','/','.']
+
+    const PrintResult = value =>{
+        if(symbols.includes(value) && val === ''){
+            return
+        }
+        
+        if(symbols.includes(value) && symbols.includes(val.slice(-1))){
+            setVal(val.slice(0,-1) + value)
+        } else{
+            setVal(val + value)
+        }
+
+        if(!symbols.includes(value)){
+            setResult(eval(val+value).toString())
+        }
+    }
+
+    const FinalValue = () => {
+        setVal(eval(val).toString())   
+    }
     const CreateDigits = () => {
         const digits = []
-        for (let i =1;i<10;i++) {
+        for(let i =1;i<10;i++) {
             digits.push(
-                <button key={i}> {i} </button>)
+                <button 
+                onClick={ () => PrintResult(i.toString())} 
+                key={i}> 
+                {i} 
+                </button>)
         }
         return digits
     }
@@ -13,22 +41,22 @@ function Calculator() {
         <div className="calc">
             <div>
                 <div className="result">
-                    <span>(0)</span>0
+                {result ? <span>({result})</span> : '' }
+                {val || 0}
                 </div>
                 <div className="operators">
-                    <button> C </button>
-                    <button> + </button>
-                    <button> - </button>
-                    <button> * </button>
-                    <button> / </button>
-                    <button> % </button>
+                    <button onClick={ () => setVal('')} > C </button>
+                    <button onClick={ () => PrintResult('+')} > + </button>
+                    <button onClick={ () => PrintResult('-')} > - </button>
+                    <button onClick={ () => PrintResult('*')} > * </button>
+                    <button onClick={ () => PrintResult('/')} > / </button>
                 </div>
 
                 <div className="digits">
                     {CreateDigits()}
-                    <button> 0 </button>
-                    <button> . </button>
-                    <button> = </button>
+                    <button onClick={ () => PrintResult('0')} > 0 </button>
+                    <button onClick={ () => PrintResult('.')}> . </button>
+                    <button onClick={ () => FinalValue()}> = </button>
                 </div>
             </div>
         </div>
